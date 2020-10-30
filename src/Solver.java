6676995d085d87aea999e9336d460f409591591a
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2020-10-30 15:59:06 +0100

    Created skeleton for assignment

---
+++ src/Solver.java
@@ -0,0 +1,26 @@
+public class Solver {
+
+    private int noOfQueens;
+    private int[] queens;
+    private int noOfSolutions;
+
+    public void findAllSolutions(int noOfQueens) {
+
+    }
+
+    private void positionQueen(int row) {
+
+    }
+
+    private boolean legal(int row, int col) {
+        return false;
+    }
+
+    private void printSolution() {
+
+    }
+
+    private String convert(int row, int col) {
+        return "";
+    }
+}
